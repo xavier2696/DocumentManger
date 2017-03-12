a30649147ebdd09ec177efcf26ef74e71d2831d8
Kelvin Xavier Munguia <xavier2696@gmail.com>
2017-03-11 18:35:12 -0600

    small fixes

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,7 @@
 class Document < ApplicationRecord
-  belongs_to :senderEmail, :class_name => 'User', :foreign_key => 'sender_id'
-  belongs_to :receiverEmal, :class_name => 'User', :foreign_key => 'receiver_id'
-  belongs_to :creatorEmail, :class_name => 'User', :foreign_key => 'creator_id'
+  belongs_to :sender, :class_name => 'User', :foreign_key => 'sender_id'
+  belongs_to :receiver, :class_name => 'User', :foreign_key => 'receiver_id'
+  belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
   belongs_to :senderStatus, :class_name => 'Status', :foreign_key => 'senderStatus_id'
   belongs_to :receiverStatus, :class_name => 'Status', :foreign_key => 'receiverStatus_id'
   has_many :images
