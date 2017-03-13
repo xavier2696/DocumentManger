ea03c1a037c05799fd8a927c98afa1780ea3bb3e
Jose Hernandez <joedhernandez95@hotmail.com>
2017-03-13 14:25:00 -0600

    verison beta del dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require_tree .
 $(document).on('turbolinks:load', function() {
  $('select').material_select();
+ $('.collapsible').collapsible();
    $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
     before_action :authenticate_user!
     
     def index
+    	@documents = Document.all
     end
     
     
