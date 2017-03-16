36636e957937f74eda8f1328e0079b92a37434e5
Jose Hernandez <joedhernandez95@hotmail.com>
2017-03-16 01:29:53 -0600

    ya se agrupan los mensajes en el dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,29 @@ class WelcomeController < ApplicationController
     def index
     	@documents = Document.all
     end
+
+    def getAmountSent
+    	count = 0
+    	@documents.each do |document|
+    		if document.sender_id == current_user.id
+    			count = count +1
+    		end
+    	end
+    	return count
+    end
+
+    def getAmountRecieved
+    	count = 0
+    	@documents.each do |document|
+    		if document.receiver_id == current_user.id
+    			count = count +1
+    		end
+    	end
+    	return count
+    end
+
+    helper_method :getAmountSent
+    helper_method :getAmountRecieved
     
     
 end
