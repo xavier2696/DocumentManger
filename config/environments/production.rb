3048523f40dcc81c52fc0f70238930d5596a39f9
Jose Pejuan <pejuan94@gmail.com>
2017-03-20 00:14:27 +0000

    Poniendo paperclip paths en production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ config.assets.digest = true
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
