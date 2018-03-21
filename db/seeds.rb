7ba6ed0987c84fadae4ddf30c12378fe4e0e9676
Nelbren <nelbren@gmail.com>
2018-03-20 20:23:42 -0600

    seeds!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Department.create("departmentName"=>"INFORMATICA")
+Department.create("departmentName"=>"INFRAESTRUCTURA")
+
+User.create("email"=>"superadmin@test.com", "password"=>"superadmin", "username"=>"Super Admin", "department_id"=>1, "position"=>"Test", "isGeneralAdmin"=>true, "isDepartmentAdmin"=>false)
+User.create("email"=>"admin_infor@test.com", "password"=>"admin_infor", "username"=>"Admin informatica", "department_id"=>1, "position"=>"Jefe", "isGeneralAdmin"=>false, "isDepartmentAdmin"=>true)
+User.create("email"=>"admin_infra@test.com", "password"=>"admin_infra", "username"=>"Admin infraestructura", "department_id"=>2, "position"=>"Jefe", "isGeneralAdmin"=>false, "isDepartmentAdmin"=>true)
+
