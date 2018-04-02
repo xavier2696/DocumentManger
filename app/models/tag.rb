1875062552fa83cd366f82e855f42a6eaf24657a
beito <beito_xp@me.com>
2018-04-02 16:39:12 -0600

    Testing for Tags Model
    Three test for the Tags module

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
     has_and_belongs_to_many :documents
+    validates :tagName, presence: true
+    validates_uniqueness_of :tagName, scope: :department_id
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
-
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "tagCreate_nil_name" do
+    tag = Tag.new(tagName: tags(:tagvacio).tagName)
+    assert !tag.valid?
+    refute tag.valid?
+    assert_match /blank/, tag.errors[:tagName].to_s
+  end
+  test "tagCreate" do
+    tag = Tag.new :tagName => tags(:tagnombre).tagName
+    assert tag.save
+    tag_copy = Tag.find(tag.id)
+    assert_equal tag.tagName, tag_copy.tagName
+    assert tag.destroy
+  end
+  test "tagCreate_same_name" do
+    tag_duplicado = Tag.new(tagName: tags(:tagrepetido).tagName)
+    assert !tag_duplicado.invalid?
+    assert_match /taken/, tag_duplicado.errors[:tagName].to_s
+  end
 end
