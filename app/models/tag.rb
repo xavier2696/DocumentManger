3745966f96b472441be65f3739b45e4694021a96
beito <beito_xp@me.com>
2018-04-03 17:38:08 -0600

    More Testing for Tags Model
    One test for the Tags module.
    Also, there is a new validation over the blank values: allow_blank

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ApplicationRecord
-    has_and_belongs_to_many :documents
+  has_and_belongs_to_many :documents
+  validates :tagName, presence: true, allow_blank: false
+  validates :department_id, presence: true
+  validates_uniqueness_of :tagName, scope: :department_id
+  # Pendiente: has_and_belongs_to_many :department
 end
